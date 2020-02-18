07d2e5207e3ce4129d2a246d01cf5b695f11bb6a
TrouBleMaKerV <782024062@qq.com>
2020-02-18 23:30:32 +0800

    xx

---
+++ src/main/java/com/nju/NjuApplication.java
@@ -0,0 +1,13 @@
+package com.nju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NjuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nju/NjuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
