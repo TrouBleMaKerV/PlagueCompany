58dc38f5bfe523973b4015fd70667e1262b9ae65
TrouBleMaKerV <782024062@qq.com>
2020-04-02 18:01:52 +0800

    提交

--- src/main/java/com/nju/NjuApplication.java
+++ src/main/java/com/nju/NjuApplication.java
@@ -2,10 +2,10 @@ package com.nju;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class NjuApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NjuApplication.class, args);
     }
